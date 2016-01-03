49f441dabc93750f87532e4a771e73bd05fdbbe8
Botev <botevmg@gmail.com>
2016-01-03 03:25:36 +0000

    Made the example to auto download and now load MNIST dataset. Ready for some comparison.

--- examples/mnist_hinton.cpp
+++ examples/mnist_hinton.cpp
@@ -1,13 +1,191 @@
 
 #include <arrayfire.h>
+#include <sys/stat.h>
 #include "metadiff.h"
+#include "curl/curl.h"
 
 namespace md = metadiff;
 namespace sym = metadiff::symbolic;
 
+const char kPathSeparator =
+#ifdef _WIN32
+        '\\';
+#else
+        '/';
+#endif
+
+struct FtpFile {
+    const char *filename;
+    FILE *stream;
+};
+
+static size_t my_fwrite(void *buffer, size_t size, size_t nmemb, void *stream)
+{
+    struct FtpFile *out=(struct FtpFile *)stream;
+    if(out && !out->stream) {
+        /* open file for writing */
+        out->stream=fopen(out->filename, "wb");
+        if(!out->stream)
+            return -1; /* failure, can't open file to write */
+    }
+    return fwrite(buffer, size, nmemb, out->stream);
+}
+
+
+inline bool exists (const std::string& name) {
+    struct stat buffer;
+    return (stat (name.c_str(), &buffer) == 0);
+}
+
+void download_mnist(std::string folder){
+    CURL *curl;
+    CURLcode res;
+    auto data = folder;
+    data += kPathSeparator;
+    data += "train-images-idx3-ubyte.gz";
+    struct FtpFile training_data={
+            data.c_str(), /* name to store the file as if successful */
+            NULL
+    };
+    auto labels = folder;
+    labels += kPathSeparator;
+    labels += "train-labels-idx1-ubyte.gz";
+    struct FtpFile training_labels={
+            labels.c_str(), /* name to store the file as if successful */
+            NULL
+    };
+
+
+    curl_global_init(CURL_GLOBAL_DEFAULT);
+
+    curl = curl_easy_init();
+    if(curl) {
+        /*
+         * You better replace the URL with one that works!
+         */
+        if(not exists(data.substr(0, data.length()-3))){
+            curl_easy_setopt(curl, CURLOPT_URL,
+                             "http://yann.lecun.com/exdb/mnist/train-images-idx3-ubyte.gz");
+            /* Define our callback to get called when there's data to be written */
+            curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, my_fwrite);
+            /* Set a pointer to our struct to pass to the callback */
+            curl_easy_setopt(curl, CURLOPT_WRITEDATA, &training_data);
+            res = curl_easy_perform(curl);
+            if(CURLE_OK != res) {
+                /* we failed */
+                fprintf(stderr, "curl told us %d\n", res);
+            }
+        }
+        if(not exists(labels.substr(0, data.length()-3))){
+            curl_easy_setopt(curl, CURLOPT_URL,
+                             "http://yann.lecun.com/exdb/mnist/train-labels-idx1-ubyte.gz");
+            /* Define our callback to get called when there's data to be written */
+            curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, my_fwrite);
+            /* Set a pointer to our struct to pass to the callback */
+            curl_easy_setopt(curl, CURLOPT_WRITEDATA, &training_labels);
+            res = curl_easy_perform(curl);
+            if(CURLE_OK != res) {
+                /* we failed */
+                fprintf(stderr, "curl told us %d\n", res);
+            }
+        }
+    }
+    /* always cleanup */
+    curl_easy_cleanup(curl);
+
+    if(training_data.stream) {
+        fclose(training_data.stream); /* close the local file */
+        int res = system(("gzip -d " + data).c_str());
+    }
+
+    if(training_labels.stream) {
+        fclose(training_labels.stream); /* close the local file */
+        int res = system(("gzip -d " + labels).c_str());
+    }
+
+    curl_global_cleanup();
+}
+
+int ReverseInt (int i){
+    unsigned char ch1, ch2, ch3, ch4;
+    ch1=i&255;
+    ch2=(i>>8)&255;
+    ch3=(i>>16)&255;
+    ch4=(i>>24)&255;
+    return((int)ch1<<24)+((int)ch2<<16)+((int)ch3<<8)+ch4;
+}
+
+const int num_images = 60000;
+const int rows = 28;
+const int cols = 28;
+
+void ReadTrainMNIST(std::string folder, float* data, int* labels){
+    std::string file_name = folder;
+    file_name += kPathSeparator;
+    file_name += "train-images-idx3-ubyte";
+    std::ifstream file(file_name,std::ios::binary);
+    if (file.is_open())
+    {
+        int magic_number=0;
+        int number_of_images=0;
+        int n_rows=0;
+        int n_cols=0;
+        file.read((char*)&magic_number,sizeof(magic_number));
+        magic_number= ReverseInt(magic_number);
+        file.read((char*)&number_of_images,sizeof(number_of_images));
+        number_of_images= ReverseInt(number_of_images);
+        file.read((char*)&n_rows,sizeof(n_rows));
+        n_rows= ReverseInt(n_rows);
+        file.read((char*)&n_cols,sizeof(n_cols));
+        n_cols= ReverseInt(n_cols);
+        std::cout << number_of_images << ", " << rows << ", " << cols << std::endl;
+        for(int i=0;i<number_of_images;++i)
+        {
+            for(int r=0;r<n_rows;++r)
+            {
+                for(int c=0;c<n_cols;++c)
+                {
+                    unsigned char temp=0;
+                    file.read((char*)&temp,sizeof(temp));
+                    data[i*28*28 + (r + c*rows)]= ((float)temp) / float(255.0);
+                }
+            }
+        }
+    }
+    file.close();
+    file_name = folder;
+    file_name += kPathSeparator;
+    file_name += "train-labels-idx1-ubyte";
+    file.open(file_name, std::ios::binary);
+    if (file.is_open())
+    {
+        int magic_number=0;
+        int number_of_images=0;
+        file.read((char*)&magic_number,sizeof(magic_number));
+        magic_number= ReverseInt(magic_number);
+        file.read((char*)&number_of_images,sizeof(number_of_images));
+        number_of_images= ReverseInt(number_of_images);
+        std::cout << number_of_images << std::endl;
+        for(int i=0;i<number_of_images;++i)
+        {
+                    unsigned char temp=0;
+                    file.read((char*)&temp,sizeof(temp));
+                    labels[i]= (int)temp;
+        }
+    }
+    file.close();
+}
+
 int main(int argc, char **argv)
 {
+    // Download and load MNIST
     std::string name = "mnist_hinton";
+    mkdir(name.c_str(), S_IRWXU | S_IRWXG | S_IROTH);
+    download_mnist(name);
+    float * data_ptr = new float[28*28*num_images]{};
+    int* labels_ptr = new int[num_images]{};
+    ReadTrainMNIST(name, data_ptr, labels_ptr);
+
     // Default to CPU
     af_backend backend = AF_BACKEND_CPU;
     // Default batch size of 1000
@@ -39,6 +217,10 @@ int main(int argc, char **argv)
         }
     }
     af::setBackend(backend);
+
+    // Transfer data to Arrayfire
+    af::array data(28*28, num_images, data_ptr, afHost);
+    af::array l_in(num_images, labels_ptr, afHost);
 
     // Create graph
     auto graph = md::create_graph();
@@ -81,6 +263,7 @@ int main(int argc, char **argv)
         updates.push_back(std::pair<md::Node, md::Node>(params[i], params[i] - learning_rate * grads[i]));
     }
 
+    name += kPathSeparator + name;
     // Print to file
     md::dagre::dagre_to_file(name + ".html", graph, {loss}, updates);
 
@@ -90,15 +273,19 @@ int main(int argc, char **argv)
 
     // Run function
     long long time = 0;
+    long long min_time = 1000 * 60 * 60 * 24;
+    long long max_time = 0;
 
-    // Number of epochs
-    int epochs = 100;
     // Number of epochs for burnout, to be discarded
     int burnout = 20;
+    // Number of epochs
+    int epochs = 100 + burnout;
 
     std::vector<af::array> data_inv = {af::randn(d[0], batch_size)};
     for(int i=0;i<epochs;i++){
+        int ind = i % (num_images / batch_size);
         // Input data
+        data_inv = {data.cols(ind*batch_size, (ind+1)*batch_size)};
         clock_t start = clock();
         auto result = train.eval(data_inv);
         clock_t end = clock();
@@ -106,11 +293,20 @@ int main(int argc, char **argv)
         std::cout << "Value: " << hv[0] << std::endl;
         std::cout << "Elapsed time: " << 1000*(double(end - start))/CLOCKS_PER_SEC << "ms" << std::endl;
         if(i >= burnout) {
-            time += (end - start);
+            auto run_time = (end - start);
+            if(run_time < min_time){
+                min_time = run_time;
+            }
+            if(run_time > max_time){
+                max_time = run_time;
+            }
+            time += run_time;
         }
     }
     md_backend.close();
 
-    std::cout << "Mean Elapsed time: " << 1000*(double (time))/(CLOCKS_PER_SEC*(epochs - burnout)) << "ms" << std::endl;
+    std::cout << "Mean run time: " << 1000*(double (time))/(CLOCKS_PER_SEC*(epochs - burnout)) << "ms" << std::endl;
+    std::cout << "Max run time: " << 1000*(double (max_time))/CLOCKS_PER_SEC << "ms" << std::endl;
+    std::cout << "Min run time: " << 1000*(double (min_time))/CLOCKS_PER_SEC << "ms" << std::endl;
     return 0;
 }
